4a0acf5563a497b6b880225d0e3fc0e9a636823a
saintYoon <gamma4000@hanmail.net>
2022-01-11 05:17:08 +0900

    neo commit

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloSpringApplication.class, args);
+		//정말힘들었지만 클론완료.,..
 	}
 
 }
